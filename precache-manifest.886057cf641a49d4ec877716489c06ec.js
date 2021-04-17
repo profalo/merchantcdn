bb75ef4d403d2343cd8d44f09f634c55ef68678c
Paradox <paradox.akshay@gmail.com>
2021-04-17 13:17:33 +0530

    init

---
+++ precache-manifest.886057cf641a49d4ec877716489c06ec.js
@@ -0,0 +1,270 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd68273eba1c4b69350b94a4b2c407c9",
+    "url": "https://cdn.jsdelivr.net/gh/profalo/merchantcdn/index.html"
+  },
+  {
+    "revision": "9c9e240d3921500e4c2b",
+    "url": "https://cdn.jsdelivr.net/gh/profalo/merchantcdn/static/css/4.6c1940b0.chunk.css"
+  },
+  {
+    "revision": "46c42c6c0f9df24da1cc",
+    "url": "https://cdn.jsdelivr.net/gh/profalo/merchantcdn/static/css/main.2e0730fb.chunk.css"
+  },
+  {
+    "revision": "8445ff695f88f68aa969",
+    "url": "https://cdn.jsdelivr.net/gh/profalo/merchantcdn/static/js/0.8707cb34.chunk.js"
+  },
+  {
+    "revision": "d74ad8086e72984965f9",
+    "url": "https://cdn.jsdelivr.net/gh/profalo/merchantcdn/static/js/10.251ea22f.chunk.js"
+  },
+  {
+    "revision": "1fca440f1a98a9ea1102",
+    "url": "https://cdn.jsdelivr.net/gh/profalo/merchantcdn/static/js/11.414bdd4c.chunk.js"
+  },
+  {
+    "revision": "79953dd2f1b9a9acd758",
+    "url": "https://cdn.jsdelivr.net/gh/profalo/merchantcdn/static/js/12.ee0c2fb7.chunk.js"
+  },
+  {
+    "revision": "b73de3b2ba59bfdb231b",
+    "url": "https://cdn.jsdelivr.net/gh/profalo/merchantcdn/static/js/13.dbb9e658.chunk.js"
+  },
+  {
+    "revision": "8b58c488c440dbdae74b",
+    "url": "https://cdn.jsdelivr.net/gh/profalo/merchantcdn/static/js/14.b7fa2f7f.chunk.js"
+  },
+  {
+    "revision": "92eece0abbf4d7233bbc",
+    "url": "https://cdn.jsdelivr.net/gh/profalo/merchantcdn/static/js/15.be52b677.chunk.js"
+  },
+  {
+    "revision": "d5246a2dc3672e32acbf",
+    "url": "https://cdn.jsdelivr.net/gh/profalo/merchantcdn/static/js/16.763d281a.chunk.js"
+  },
+  {
+    "revision": "32c802e49abb63e9a679",
+    "url": "https://cdn.jsdelivr.net/gh/profalo/merchantcdn/static/js/17.0b810965.chunk.js"
+  },
+  {
+    "revision": "aebe363ac28918063878",
+    "url": "https://cdn.jsdelivr.net/gh/profalo/merchantcdn/static/js/18.ec70fbab.chunk.js"
+  },
+  {
+    "revision": "706704e748cf1e227282",
+    "url": "https://cdn.jsdelivr.net/gh/profalo/merchantcdn/static/js/19.14f14409.chunk.js"
+  },
+  {
+    "revision": "c6c5c4ea7eba73aee7e9",
+    "url": "https://cdn.jsdelivr.net/gh/profalo/merchantcdn/static/js/20.7d74dd19.chunk.js"
+  },
+  {
+    "revision": "549223ab135998a0abb5",
+    "url": "https://cdn.jsdelivr.net/gh/profalo/merchantcdn/static/js/21.6cd9bcd3.chunk.js"
+  },
+  {
+    "revision": "ab09aa2f33f102aef15c",
+    "url": "https://cdn.jsdelivr.net/gh/profalo/merchantcdn/static/js/22.af5c0287.chunk.js"
+  },
+  {
+    "revision": "b4c8d7a303e18efabfea",
+    "url": "https://cdn.jsdelivr.net/gh/profalo/merchantcdn/static/js/23.dd6fe130.chunk.js"
+  },
+  {
+    "revision": "3f9b45d17c4840709790",
+    "url": "https://cdn.jsdelivr.net/gh/profalo/merchantcdn/static/js/24.732499f8.chunk.js"
+  },
+  {
+    "revision": "9576a4a4f9246c68e49b",
+    "url": "https://cdn.jsdelivr.net/gh/profalo/merchantcdn/static/js/25.0d519b9d.chunk.js"
+  },
+  {
+    "revision": "348a238937d8714d4a6c",
+    "url": "https://cdn.jsdelivr.net/gh/profalo/merchantcdn/static/js/26.1bd5feb8.chunk.js"
+  },
+  {
+    "revision": "955e5a57c52b7a779b62",
+    "url": "https://cdn.jsdelivr.net/gh/profalo/merchantcdn/static/js/27.fd94a4f8.chunk.js"
+  },
+  {
+    "revision": "2aec48d5dae941a25317",
+    "url": "https://cdn.jsdelivr.net/gh/profalo/merchantcdn/static/js/28.7073234f.chunk.js"
+  },
+  {
+    "revision": "e641b2df295a28337e5e",
+    "url": "https://cdn.jsdelivr.net/gh/profalo/merchantcdn/static/js/29.1c3acf3e.chunk.js"
+  },
+  {
+    "revision": "4ee95f8d2edc055ff731",
+    "url": "https://cdn.jsdelivr.net/gh/profalo/merchantcdn/static/js/30.47d297f7.chunk.js"
+  },
+  {
+    "revision": "c654ee9747b1135aab21",
+    "url": "https://cdn.jsdelivr.net/gh/profalo/merchantcdn/static/js/31.dd05d388.chunk.js"
+  },
+  {
+    "revision": "bc089a01f7afa5a348d7",
+    "url": "https://cdn.jsdelivr.net/gh/profalo/merchantcdn/static/js/32.81595359.chunk.js"
+  },
+  {
+    "revision": "5e8106124bfd8a77ae43",
+    "url": "https://cdn.jsdelivr.net/gh/profalo/merchantcdn/static/js/33.da63a7d5.chunk.js"
+  },
+  {
+    "revision": "286d088ebef438df1d76",
+    "url": "https://cdn.jsdelivr.net/gh/profalo/merchantcdn/static/js/34.2ff5ca3c.chunk.js"
+  },
+  {
+    "revision": "30be4a2fd694de1c0786",
+    "url": "https://cdn.jsdelivr.net/gh/profalo/merchantcdn/static/js/35.d585b92b.chunk.js"
+  },
+  {
+    "revision": "5ac54833e07ae09e334e",
+    "url": "https://cdn.jsdelivr.net/gh/profalo/merchantcdn/static/js/36.3802eaf2.chunk.js"
+  },
+  {
+    "revision": "385cd754aadd546ad016",
+    "url": "https://cdn.jsdelivr.net/gh/profalo/merchantcdn/static/js/37.4e45563e.chunk.js"
+  },
+  {
+    "revision": "0c8e2e2acad39bec0a8a",
+    "url": "https://cdn.jsdelivr.net/gh/profalo/merchantcdn/static/js/38.9a9f946c.chunk.js"
+  },
+  {
+    "revision": "d6c128aed22c19e39f3b",
+    "url": "https://cdn.jsdelivr.net/gh/profalo/merchantcdn/static/js/39.2ac80a2c.chunk.js"
+  },
+  {
+    "revision": "9c9e240d3921500e4c2b",
+    "url": "https://cdn.jsdelivr.net/gh/profalo/merchantcdn/static/js/4.9ee96b4b.chunk.js"
+  },
+  {
+    "revision": "e518a342f85bd533a36e9666f57514e0",
+    "url": "https://cdn.jsdelivr.net/gh/profalo/merchantcdn/static/js/4.9ee96b4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0931e3bd38a506dce3ea",
+    "url": "https://cdn.jsdelivr.net/gh/profalo/merchantcdn/static/js/40.b2f42957.chunk.js"
+  },
+  {
+    "revision": "5af18b4c92dea34351a0",
+    "url": "https://cdn.jsdelivr.net/gh/profalo/merchantcdn/static/js/41.048de25e.chunk.js"
+  },
+  {
+    "revision": "839ca3170889b46f0ead",
+    "url": "https://cdn.jsdelivr.net/gh/profalo/merchantcdn/static/js/42.5352c0aa.chunk.js"
+  },
+  {
+    "revision": "7ad4eec2a8ffc3dd8022",
+    "url": "https://cdn.jsdelivr.net/gh/profalo/merchantcdn/static/js/43.c1f734e3.chunk.js"
+  },
+  {
+    "revision": "e68cdc64ba276c3044d4",
+    "url": "https://cdn.jsdelivr.net/gh/profalo/merchantcdn/static/js/44.1f1ad80e.chunk.js"
+  },
+  {
+    "revision": "58966be1b8fb8ede2da1",
+    "url": "https://cdn.jsdelivr.net/gh/profalo/merchantcdn/static/js/45.9c37a58c.chunk.js"
+  },
+  {
+    "revision": "5d54bdf7850e2f9fdb92",
+    "url": "https://cdn.jsdelivr.net/gh/profalo/merchantcdn/static/js/46.22f11997.chunk.js"
+  },
+  {
+    "revision": "8de3af5c57d3bcc85e9b",
+    "url": "https://cdn.jsdelivr.net/gh/profalo/merchantcdn/static/js/47.778090ec.chunk.js"
+  },
+  {
+    "revision": "23bda3a870e5595bc6bc",
+    "url": "https://cdn.jsdelivr.net/gh/profalo/merchantcdn/static/js/48.3807128c.chunk.js"
+  },
+  {
+    "revision": "19de7ba77d2529ef107d",
+    "url": "https://cdn.jsdelivr.net/gh/profalo/merchantcdn/static/js/49.d7e7567a.chunk.js"
+  },
+  {
+    "revision": "b7fe218513b03f26518e",
+    "url": "https://cdn.jsdelivr.net/gh/profalo/merchantcdn/static/js/5.9ad194ce.chunk.js"
+  },
+  {
+    "revision": "f88cc5ba8dd69835c2b0",
+    "url": "https://cdn.jsdelivr.net/gh/profalo/merchantcdn/static/js/50.3a4be7c0.chunk.js"
+  },
+  {
+    "revision": "1d39f6914c5b355fa2d0",
+    "url": "https://cdn.jsdelivr.net/gh/profalo/merchantcdn/static/js/51.4d44ba1a.chunk.js"
+  },
+  {
+    "revision": "f22127a223fa7e1d6d56",
+    "url": "https://cdn.jsdelivr.net/gh/profalo/merchantcdn/static/js/52.e40a0e54.chunk.js"
+  },
+  {
+    "revision": "374ac1db13aadb4623e2",
+    "url": "https://cdn.jsdelivr.net/gh/profalo/merchantcdn/static/js/53.52b5ee2b.chunk.js"
+  },
+  {
+    "revision": "383899e60ed69a9a53414c6836d91ab5",
+    "url": "https://cdn.jsdelivr.net/gh/profalo/merchantcdn/static/js/53.52b5ee2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3224e51e2a39b75e42cc",
+    "url": "https://cdn.jsdelivr.net/gh/profalo/merchantcdn/static/js/54.c8c56c82.chunk.js"
+  },
+  {
+    "revision": "0335025d74cc0514fe95",
+    "url": "https://cdn.jsdelivr.net/gh/profalo/merchantcdn/static/js/55.4e2c7f40.chunk.js"
+  },
+  {
+    "revision": "85f0f7f3075080f91eaa",
+    "url": "https://cdn.jsdelivr.net/gh/profalo/merchantcdn/static/js/56.f2277778.chunk.js"
+  },
+  {
+    "revision": "e07736a3ed1e1bb18ecc",
+    "url": "https://cdn.jsdelivr.net/gh/profalo/merchantcdn/static/js/57.6a18922d.chunk.js"
+  },
+  {
+    "revision": "1ac8f574cd19d09eece7",
+    "url": "https://cdn.jsdelivr.net/gh/profalo/merchantcdn/static/js/58.786eea7a.chunk.js"
+  },
+  {
+    "revision": "def1b0071abb3077bc7c",
+    "url": "https://cdn.jsdelivr.net/gh/profalo/merchantcdn/static/js/59.ddd5a6a4.chunk.js"
+  },
+  {
+    "revision": "383899e60ed69a9a53414c6836d91ab5",
+    "url": "https://cdn.jsdelivr.net/gh/profalo/merchantcdn/static/js/59.ddd5a6a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57fecf5cd1eda66955a5",
+    "url": "https://cdn.jsdelivr.net/gh/profalo/merchantcdn/static/js/6.5b23e129.chunk.js"
+  },
+  {
+    "revision": "d395be42e44472521627",
+    "url": "https://cdn.jsdelivr.net/gh/profalo/merchantcdn/static/js/7.62f5a436.chunk.js"
+  },
+  {
+    "revision": "13fae4d9946b017f99c3",
+    "url": "https://cdn.jsdelivr.net/gh/profalo/merchantcdn/static/js/8.4fef17f0.chunk.js"
+  },
+  {
+    "revision": "7faee59c7722fb6216f9",
+    "url": "https://cdn.jsdelivr.net/gh/profalo/merchantcdn/static/js/9.024b840e.chunk.js"
+  },
+  {
+    "revision": "46c42c6c0f9df24da1cc",
+    "url": "https://cdn.jsdelivr.net/gh/profalo/merchantcdn/static/js/main.6e36bc4f.chunk.js"
+  },
+  {
+    "revision": "a6617fdece5adbcd0f86",
+    "url": "https://cdn.jsdelivr.net/gh/profalo/merchantcdn/static/js/polyfills-dom.db8beab1.chunk.js"
+  },
+  {
+    "revision": "90b6b156a5f62810eddc9189ae0e43d4",
+    "url": "https://cdn.jsdelivr.net/gh/profalo/merchantcdn/static/js/polyfills-dom.db8beab1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9dc256f4006b055e1903",
+    "url": "https://cdn.jsdelivr.net/gh/profalo/merchantcdn/static/js/runtime-main.bd849a99.js"
+  }
+]);
